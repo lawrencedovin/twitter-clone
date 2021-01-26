e4c5b4e0297fb7c056ca1ccf8d7983b577cb56c9
Lawrence Dovin <lawrencedovin@gmail.com>
2021-01-26 14:15:37 -0500

    connected edit profile form to route

--- app.py
+++ app.py
@@ -210,7 +210,7 @@ def stop_following(follow_id):
 
 
 @app.route('/users/profile/<int:user_id>', methods=["GET", "POST"])
-def profile():
+def profile(user_id):
     """Update profile for current user."""
     # IMPLEMENT THIS
     if not g.user:
@@ -221,7 +221,21 @@ def profile():
     form = UserEditForm(obj=user)
 
     if form.validate_on_submit():
-        
+        # user = User.authenticate(form.username.data,
+        #                          form.password.data)
+        # if user:
+        user.username = form.username.data
+        user.email = form.email.data
+        user.image_url = form.image_url.data
+        user.header_image_url = form.header_image_url.data
+        user.bio = form.bio.data
+
+        db.session.commit()
+        return redirect(f'/users/{g.user.id}')
+    
+    else:
+        return render_template('users/edit.html', user=user, form=form)
+
 
 
 @app.route('/users/delete', methods=["POST"])
