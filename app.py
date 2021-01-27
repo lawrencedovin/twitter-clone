3fd69f9294c58fc5a11067637f18a7fd0e8e853a
Lawrence Dovin <lawrencedovin@gmail.com>
2021-01-26 20:31:19 -0500

    Added filtering for messages by following users and own user

--- app.py
+++ app.py
@@ -326,8 +326,11 @@ def homepage():
     """
 
     if g.user:
+
+        following_ids = [following_user.id for following_user in g.user.following] + [g.user.id]
         messages = (Message
                     .query
+                    .filter(Message.user_id.in_(following_ids))
                     .order_by(Message.timestamp.desc())
                     .limit(100)
                     .all())
