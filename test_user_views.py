453a8acc34aedcd077a8700c709950660cf1ceb5
Lawrence Dovin <lawrencedovin@gmail.com>
2021-01-28 12:57:24 -0500

    Added initial test user views

---
+++ test_user_views.py
@@ -0,0 +1,57 @@
+"""User View tests."""
+
+# run these tests like:
+#
+#    FLASK_ENV=production python -m unittest test_message_views.py
+
+
+import os
+from unittest import TestCase
+
+from models import db, connect_db, Message, User, Likes, Follows
+from bs4 import BeautifulSoup
+
+# BEFORE we import our app, let's set an environmental variable
+# to use a different database for tests (we need to do this
+# before we import our app, since that will have already
+# connected to the database
+
+os.environ['DATABASE_URL'] = "postgresql:///warbler-test"
+
+
+# Now we can import app
+
+from app import app, CURR_USER_KEY
+
+# Create our tables (we do this here, so we only create the tables
+# once for all tests --- in each test, we'll delete the data
+# and create fresh new clean test data
+
+db.create_all()
+
+# Don't have WTForms use CSRF at all, since it's a pain to test
+
+app.config['WTF_CSRF_ENABLED'] = False
+
+class MessageViewTestCase(TestCase):
+    """Test views for messages."""
+
+    def setUp(self):
+        """Create test client, add sample data."""
+
+        db.drop_all()
+        db.create_all()
+
+        self.client = app.test_client()
+
+        self.test_user = User.signup("test_user", "test_user@gmail.com", "123", None)
+        self.user1 = User.signup("user1", "user1@gmail.com", "password", None)
+        self.user2 = User.signup("user2", "user2@gmail.com", "password", None)
+        self.user3 = User.signup("user3", "user3@gmail.com", "password", None)
+        self.user4 = User.signup("user4", "user4@gmail.com", "password", None)
+        db.session.commit()
+
+    def tearDown(self):
+        resp = super().tearDown()
+        db.session.rollback()
+        return resp
